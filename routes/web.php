59cefa217ef73b6583028c57338558dad1978b52
Alex Smirnov <workpost@amurlife.ru>
2018-07-23 13:42:51 +0900

    top-head menu

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.home');
 });
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
